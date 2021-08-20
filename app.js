8db2ea6e2084382f99b2294ae93ebb2665a25858
AmitSheth9 <amit.sheth9@gmail.com>
2021-08-20 15:30:26 -0700

    reset button added and css

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 import { getRandomThrow } from './get-random-throw.js';
 import { didUserWin } from './utils.js';
 const buttonEl = document.querySelector('#submit');
-const winEl = document.querySelector('#win');
-const looseEl = document.querySelector('#loose');
-const drawEl = document.querySelector('#draw');
 const totalWinEl = document.querySelector('#totalWin');
 const totalLoseEl = document.querySelector('#totalLost');
 const totalDrawEl = document.querySelector('#totalDraw');
 const computerThrow = document.querySelector('#computer-throw');
+const winLoseDraw = document.querySelector('#winlosedraw');
+const resetButton = document.querySelector('#resetbutton');
 
 // console.log(
 // 	buttonEl,
@@ -28,33 +27,36 @@ let draws = 0;
 
 // set event listeners
 buttonEl.addEventListener('click', () => {
-	const computerChoice = getRandomThrow();
+    const computerChoice = getRandomThrow();
 
-	computerThrow.textContent = `The computer threw: ${computerChoice}`;
+    computerThrow.textContent = `The computer threw: ${computerChoice}`;
 
-	const selectedInputEl = document.querySelector('input:checked');
+    const selectedInputEl = document.querySelector('input:checked');
 
-	const userWon = didUserWin(selectedInputEl.value, computerChoice);
+    const userWon = didUserWin(selectedInputEl.value, computerChoice);
 
-	if (userWon === 'win') {
-		wins++;
-		winEl.textContent = 'You won!';
-	} else if (userWon === 'lose') {
-		losses++;
-		looseEl.textContent = 'You Lose!';
-	} else {
-		draws++;
-		drawEl.textContent = `It's a Draw!`;
-	}
+    if (userWon === 'win') {
+        wins++;
+        winLoseDraw.textContent = 'You won!';
+    } else if (userWon === 'lose') {
+        losses++;
+        winLoseDraw.textContent = 'You Lose!';
+    } else {
+        draws++;
+        winLoseDraw.textContent = `It's a Draw!`;
+    }
 
-	totalDrawEl.textContent = `total draws ${draws}`;
-	totalWinEl.textContent = `total wins ${wins}`;
-	totalLoseEl.textContent = `total losses ${losses}`;
+    totalDrawEl.textContent = `total draws ${draws}`;
+    totalWinEl.textContent = `total wins ${wins}`;
+    totalLoseEl.textContent = `total losses ${losses}`;
 
 	// get user input
 	// use user input to update state
 	// update DOM to reflect the new state
 });
 
+resetButton.addEventListener('click', () => {
+    location.reload();
+});
 //TO do:  Clear you won, you lose message each time we run the game.
 //To do:  Make sure that when you hit submit with nothing selected that you throw a prompt
